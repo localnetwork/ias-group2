838921b578aaf8a006ef9a077e6e9afcd434eba6
localnetwork <diomenikep@gmail.com>
2024-11-18 13:13:22 +0800

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,56 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: process.env.NODE_ENV === "production" ? "export" : "standalone",
-  reactStrictMode: true,
+  reactStrictMode: false,
+  images: {
+    unoptimized: true, // Disable Image Optimization API
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Cache-control",
+            value: "public, max-age=31536000, immutable",
+          },
+          {
+            key: "Expires",
+            value: new Date(Date.now() + 31536000000).toUTCString(),
+          },
+          {
+            key: "Content-Security-Policy",
+            value:
+              "script-src-attr 'self' 'unsafe-inline'; style-src-attr 'self' 'unsafe-inline'; frame-ancestors 'self'",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "geolocation=self",
+          },
+          {
+            key: "X-xss-protection",
+            value: "1; mode=block",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
